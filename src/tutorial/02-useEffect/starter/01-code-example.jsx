611b9323a8dcb78e026abd5c0a78b88cd9e9aebb
York Yu <n11453559@gmail.com>
2025-09-03 22:00:05 +1000

    Refactor import statements and clean up CodeExample component

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const CodeExample = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
+
+  const sayHello = () => {
+    console.log('hello world')
+  }
+  sayHello()
+  console.log('render component')
 
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
-  );
-};
-export default CodeExample;
+  )
+}
+export default CodeExample
