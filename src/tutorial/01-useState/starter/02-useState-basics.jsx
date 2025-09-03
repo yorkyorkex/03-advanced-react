9fc234393413f32fa0c138a33491607a00c90518
York Yu <n11453559@gmail.com>
2025-09-03 13:45:09 +1000

    Implement useState in UseStateBasics component for count management and button functionality

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,21 @@
-const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateBasics;
+const UseStateBasics = () => {
+  const [count, setCount] = useState(0)
+  console.log(count)
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <>
+      <h2>useState basics</h2>
+      <h1>{count}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        increase
+      </button>
+    </>
+  )
+}
+
+export default UseStateBasics
