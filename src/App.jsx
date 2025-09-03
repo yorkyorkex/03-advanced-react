09a399ae2e4bd91c7f92be93fbd658ad338e0922
York Yu <n11453559@gmail.com>
2025-09-03 22:38:04 +1000

    Refactor import statements and clean up MultipleEffects component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@
 //import Starter from './tutorial/01-useState/starter/04-useState-object'
 //import Starter from './tutorial/01-useState/starter/05-useState-gotcha'
 //impoerrt Starter from './tutorial/01-useState/starter/06-useState-counter'
-import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
+//import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
 /* import Starter from './tutorial/02-useEffect/starter/02-useEffect-cleanup' */
-/* import Starter from './tutorial/02-useEffect/starter/03-useEffect-fetch-data' */
-/* import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns' */
+import Starter from './tutorial/02-useEffect/starter/03-multiple-effects'
+//import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns'
 /* import Starter from './tutorial/03-conditional-rendering/starter/02-short-circuit' */
 /* import Starter from './tutorial/03-conditional-rendering/starter/03-show-hide' */
 /* import Starter from './tutorial/04-forms/starter/01-controlled-inputs' */
