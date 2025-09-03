563a81963f621509fb7abbf0da771d0a1826b0e2
York Yu <n11453559@gmail.com>
2025-09-03 14:38:19 +1000

    Implement useState for managing person details in UseStateObject component

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,26 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [name, setName] = useState('john')
+  const [age, setAge] = useState(24)
+  const [hobby, setHobby] = useState('football')
+  const displayPerson = () => {
+    setName('peter')
+    setAge(30)
+    setHobby('tennis')
+    console.log(name, age, hobby)
+  }
+  return (
+    <>
+      <h3></h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{hobby}</h3>
+      <button type="button" className="btn" onClick={displayPerson}>
+        change person
+      </button>
+    </>
+  )
+}
 
-export default UseStateObject;
+export default UseStateObject
