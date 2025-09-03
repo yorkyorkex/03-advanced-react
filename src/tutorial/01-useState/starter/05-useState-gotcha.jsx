3161acbdad9906f4462b5c2d3305bdd8256adb05
York Yu <n11453559@gmail.com>
2025-09-03 21:35:32 +1000

    Refactor handleClick to use functional state update in UseStateGotcha component

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -4,8 +4,12 @@ const UseStateGotcha = () => {
   const [value, setValue] = useState(0)
 
   const handleClick = () => {
-    setValue(value + 1)
-    console.log(value)
+    setValue((currentState) => {
+      const newState = currentState + 1
+
+      return newState
+    })
+    // console.log(value)
   }
 
   return (
