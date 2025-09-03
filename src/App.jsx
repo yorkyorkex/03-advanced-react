dfe0bc0d2813f66e7f039753e888101508e40ead
York Yu <n11453559@gmail.com>
2025-09-03 14:02:18 +1000

    Update import statement in App component to use useState-array example

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* import Starter from './tutorial/01-useState/starter/01-error-example' */
-import Starter from './tutorial/01-useState/starter/02-useState-basics'
-/* import Starter from './tutorial/01-useState/starter/03-useState-array'
-import Starter from './tutorial/01-useState/starter/04-useState-object' */
+/* import Starter from './tutorial/01-useState/starter/02-useState-basics' */
+import Starter from './tutorial/01-useState/starter/03-useState-array'
+/* import Starter from './tutorial/01-useState/starter/04-useState-object' */
 
 import Final from './tutorial/07-useRef/final/01-useRef-basics'
 
