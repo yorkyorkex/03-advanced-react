ff56220f1afe1592dd197bcd043f5c5c2a556577
York Yu <n11453559@gmail.com>
2025-09-04 10:18:14 +1000

    Refactor MultipleReturnsBasics component to implement loading and error states

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 const MultipleReturnsBasics = () => {
-  return <h2>Multiple Returns Basics</h2>;
-};
-export default MultipleReturnsBasics;
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+      setError(true)
+    }, 3000)
+  }, [])
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+  if (error) {
+    return <h2>Error...</h2>
+  }
+  return <h2>Multiple Returns Basics</h2>
+}
+export default MultipleReturnsBasics
