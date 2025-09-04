6509e1b1a2bf67e74cd3cfde4a3ee560d7ff5943
York Yu <n11453559@gmail.com>
2025-09-04 11:56:53 +1000

    Refactor ShortCircuitOverview component to define codeExample variable for improved readability

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 const ShortCircuitOverview = () => {
   const [text, setText] = useState('')
   const [name, setName] = useState('john')
-
+  const codeExample = text || 'default text'
   return (
     <>
       <h2>short circuit overview</h2>
@@ -11,6 +11,7 @@ const ShortCircuitOverview = () => {
       <h3>False And {text && 'default text'}</h3>
       <h3>True OR {name || 'default name'}</h3>
       <h3>True And {name && 'default name'}</h3>
+      {codeExample}
     </>
   )
 }
