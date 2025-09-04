fa17665c9fbe54219149cd2e345b2ca2957325d4
York Yu <n11453559@gmail.com>
2025-09-04 10:58:36 +1000

    Refactor MultipleReturnsFetchData component to implement user fetching logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@
 //import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
 /* import Starter from './tutorial/02-useEffect/starter/02-useEffect-cleanup' */
 //import Starter from './tutorial/02-useEffect/starter/04-fetch-data'
-import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
+import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
 /* import Starter from './tutorial/03-conditional-rendering/starter/02-short-circuit' */
 /* import Starter from './tutorial/03-conditional-rendering/starter/03-show-hide' */
 /* import Starter from './tutorial/04-forms/starter/01-controlled-inputs' */
--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,21 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
-};
-export default MultipleReturnsFetchData;
+  const [users, setUsers] = useState(null)
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const resp = await fetch(url)
+        const user = await resp.json()
+        //setUsers(user)
+        console.log(user)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchUser()
+  }, [])
+  return <h2>Fetch Data </h2>
+}
+export default MultipleReturnsFetchData
