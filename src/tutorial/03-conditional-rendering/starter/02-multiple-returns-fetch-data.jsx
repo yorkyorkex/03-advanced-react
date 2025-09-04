28240030325b1a3fa062238f200a8c6fccdfd92c
York Yu <n11453559@gmail.com>
2025-09-04 11:01:43 +1000

    Refactor MultipleReturnsFetchData component to manage loading and error states

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -3,6 +3,8 @@ const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
   const [users, setUsers] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
   useEffect(() => {
     const fetchUser = async () => {
       try {
@@ -16,6 +18,15 @@ const MultipleReturnsFetchData = () => {
     }
     fetchUser()
   }, [])
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (isError) {
+    return <h2>Error fetching user data</h2>
+  }
+
   return <h2>Fetch Data </h2>
 }
 export default MultipleReturnsFetchData
