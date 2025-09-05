09f6ded53d6ba1953e80978bbf3d7b6c49e566ea
York Yu <n11453559@gmail.com>
2025-09-05 22:04:41 +1000

    Add Navbar component and associated styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@
 //impoerrt Starter from './tutorial/01-useState/starter/06-useState-counter'
 //import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
 /* import Starter from './tutorial/02-useEffect/starter/02-useEffect-cleanup' */
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
+//import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
 //import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge'
 /* import Starter from './tutorial/03-conditional-rendering/starter/02-short-circuit' */
 /* import Starter from './tutorial/03-conditional-rendering/starter/03-show-hide' */
-/* import Starter from './tutorial/04-forms/starter/01-controlled-inputs' */
+import Starter from './tutorial/04-project-structure/starter/Navbar'
 /* import Starter from './tutorial/04-forms/starter/02-multiple-inputs' */
 /* import Starter from './tutorial/05-useRef/starter/01-useRef-basics' */
 /* import Starter from './tutorial/05-useRef/starter/02-useRef-timer' */
