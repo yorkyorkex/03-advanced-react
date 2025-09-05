1d418bf30a020aa3fce27e472b68a6c265b9cbf9
York Yu <n11453559@gmail.com>
2025-09-05 20:53:15 +1000

    Refactor toggle functionality in ToggleChallenge component for improved clarity

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -3,19 +3,19 @@ import { useState } from 'react'
 const ToggleChallenge = () => {
   const [showAlert, setShowAlert] = useState(false)
 
-  const toggleAlert = () => {
+  /*   const toggleAlert = () => {
     if (showAlert) {
       setShowAlert(false)
       return
     } else {
       setShowAlert(true)
     }
-  }
+  } */
 
   return (
     <div>
       <h2>toggle challenge</h2>
-      <button className="btn" onClick={toggleAlert}>
+      <button className="btn" onClick={() => setShowAlert(!showAlert)}>
         {showAlert ? 'hide' : 'show'}
       </button>
       {showAlert && <Alert />}
