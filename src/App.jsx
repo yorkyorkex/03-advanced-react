674532143d7c0c2d348f304cab156ff752ac7f1d
York Yu <n11453559@gmail.com>
2025-09-05 22:19:53 +1000

    Add Example component with FirstComponent and SecondComponent

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@
 //import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge'
 /* import Starter from './tutorial/03-conditional-rendering/starter/02-short-circuit' */
 /* import Starter from './tutorial/03-conditional-rendering/starter/03-show-hide' */
+import Example from './tutorial/04-project-structure/starter/Example'
 import Starter from './tutorial/04-project-structure/starter/Navbar'
 
 import { About, Home } from './tutorial/04-project-structure/starter/Pages'
@@ -30,6 +31,7 @@ function App() {
       <Starter />
       <Home />
       <About />
+      <Example />
     </div>
   )
 }
---
+++ src/tutorial/04-project-structure/starter/Example/FirstComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function FirstComponent() {
+  return <div>FirstComponent</div>
+}
+
+export default FirstComponent
