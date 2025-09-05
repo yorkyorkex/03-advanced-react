5baf7d93074af4b45bb23c1aee9342307ce828d5
York Yu <n11453559@gmail.com>
2025-09-05 21:08:13 +1000

    Refactor CleanupFunction component to implement toggle functionality and cleanup effect

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,36 @@
+import React from 'react'
+import { use } from 'react'
+import { useState, useEffect } from 'react'
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
-};
+  const [toggle, setToggle] = useState(false)
 
-export default CleanupFunction;
+  useEffect(() => {
+    console.log('Effect ran')
+    return () => {
+      console.log('Cleanup')
+    }
+  }, [toggle])
+
+  return (
+    <div>
+      <h2>cleanup function</h2>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle component
+      </button>
+
+      {toggle && <RandomComponent />}
+    </div>
+  )
+}
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log('Random component mounted')
+    return () => {
+      console.log('Random component unmounted')
+    }
+  }, [])
+  return <h2>Random component</h2>
+}
+
+export default CleanupFunction
