63027e99a3e4e6e74026e5683e6a42edf532f7b4
York Yu <n11453559@gmail.com>
2025-09-07 18:56:59 +1000

    Update App component to use form-data starter and enhance form handling with FormData API

--- src/tutorial/06-forms/starter/05-form-data.jsx
+++ src/tutorial/06-forms/starter/05-form-data.jsx
@@ -1,47 +1,60 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const UncontrolledInputs = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+    console.log(e.currentTarget)
+    const formData = new FormData(e.currentTarget)
+    console.log(formData)
+    console.log(...formData)
+    const name = formData.get('name')
+    console.log(name)
+    const email = formData.get('email')
+    console.log(email)
+    const password = formData.get('password')
+    console.log(password)
+    console.log(name, email, password)
+    setValue(value + 1)
+    e.currentTarget.reset()
+  }
   return (
     <div>
-      <form className='form' onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Form Data API</h4>
         {/* name */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' name='name' />
+          <input type="text" className="form-input" id="name" name="name" />
         </div>
         {/* email */}
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
             Email
           </label>
-          <input type='email' className='form-input' id='email' name='email' />
+          <input type="email" className="form-input" id="email" name="email" />
         </div>
-        {/* email */}
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
+        {/* password */}
+        <div className="form-row">
+          <label htmlFor="password" className="form-label">
             Password
           </label>
           <input
-            type='password'
-            className='form-input'
-            id='password'
-            name='password'
+            type="password"
+            className="form-input"
+            id="password"
+            name="password"
           />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
     </div>
-  );
-};
-export default UncontrolledInputs;
+  )
+}
+export default UncontrolledInputs
