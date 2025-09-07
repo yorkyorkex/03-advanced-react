046f1c6abbc625d941ffceb05853f1bdf2ff884a
York Yu <n11453559@gmail.com>
2025-09-07 19:46:44 +1000

    Update UseRefBasics component to implement useRef for input handling and clean up formatting

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -1,31 +1,48 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'
 
 const UseRefBasics = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
+  const refContainer = useRef(null)
+  console.log(refContainer)
 
+  useEffect(() => {
+    console.log('useEffect')
+    console.log(refContainer.current)
+    refContainer.current.focus()
+  }, [])
   const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+    const name = refContainer.current.value
+    console.log('form submitted')
+    console.log(name)
+    // setValue(name)
+    // refContainer.current.value = ''
+  }
 
   return (
     <div>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input type='text' id='name' className='form-input' />
+          <input
+            type="text"
+            id="name"
+            className="form-input"
+            ref={refContainer}
+          />
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       <h1>value : {value}</h1>
-      <button onClick={() => setValue(value + 1)} className='btn'>
+      <button onClick={() => setValue(value + 1)} className="btn">
         increase
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default UseRefBasics;
+export default UseRefBasics
