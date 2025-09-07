9095055d124cc540f3f04b53c55d468848eb0c96
York Yu <n11453559@gmail.com>
2025-09-07 12:38:48 +1000

    Implement controlled inputs for name and email in ControlledInputs component

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,48 @@
+import { useState } from 'react'
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
-};
-export default ControlledInputs;
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // do something
+    console.log(name, email)
+  }
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            Name :
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="form-input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            Email :
+          </label>
+
+          <input
+            type="email"
+            id="email"
+            className="form-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+      <h2>Controlled Inputs</h2>
+    </div>
+  )
+}
+export default ControlledInputs
