51c96dbd7dd6d544e8d4cd08c0b89b29f67eab03
York Yu <n11453559@gmail.com>
2025-09-07 20:17:48 +1000

    Refactor UseRefBasics component to enhance focus handling and manage component mount state

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -3,13 +3,26 @@ import { useEffect, useRef, useState } from 'react'
 const UseRefBasics = () => {
   const [value, setValue] = useState(0)
   const refContainer = useRef(null)
+  const isMounted = useRef(false)
   console.log(refContainer)
 
-  useEffect(() => {
+  /* useEffect(() => {
     console.log('useEffect')
     console.log(refContainer.current)
     refContainer.current.focus()
-  }, [])
+  }, []) */
+  useEffect(() => {
+    refContainer.current.focus()
+  })
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true
+      return
+    }
+    console.log('useEffect')
+    console.log(refContainer.current)
+    refContainer.current.focus()
+  }, [value])
   const handleSubmit = (e) => {
     e.preventDefault()
     const name = refContainer.current.value
